06623227246459dca4405da30414eadbe0873898
jinwoo <perkyjw@gmail.com>
2019-12-23 15:45:11 +0900

    mailprogramming

---
+++ src/mail-programming/191218-array-sort.js
@@ -0,0 +1,33 @@
+function divide(array, left, right, pivot) {
+  while (left <= right) {
+    while (array[left] < pivot) {
+      left++;
+    }
+    while (array[right] > pivot) {
+      right--;
+    }
+    if (left <= right) {
+      [array[left], array[right]] = [array[right], array[left]];
+      left++;
+      right--;
+    }
+  }
+
+  return left;
+}
+
+function quickSort(array, left = 0, right = array.length - 1) {
+  if (left >= right) {
+    return;
+  }
+
+  const mid = Math.floor((left + right) / 2);
+  const pivot = array[mid];
+  const partition = divide(array, left, right, pivot);
+  quickSort(array, left, partition - 1);
+  quickSort(array, partition, right);
+
+  return array;
+}
+
+console.log(quickSort([1, 12, 5, 26, 7, 14, 3, 7, 2]));
